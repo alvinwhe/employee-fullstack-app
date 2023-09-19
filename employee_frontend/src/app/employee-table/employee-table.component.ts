609fd924bf92f82bb9b3b934b2b5df3592fb34e9
Alvin He <awhe@uwaterloo.ca>
2023-09-19 01:10:48 -0600

    Created employee form modal to handle edit and add employee, hooked up button with modal and api calls

--- employee_frontend/src/app/employee-table/employee-table.component.ts
+++ employee_frontend/src/app/employee-table/employee-table.component.ts
@@ -1,7 +1,9 @@
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
+import { MatDialog } from '@angular/material/dialog';
+import { EmployeeFormModalComponent } from 'src/app/employee-form-modal/employee-form-modal.component';
 
 export interface Employee {
   id: number;
@@ -19,25 +21,93 @@ export interface Employee {
 export class EmployeeTableComponent {
   employeeList: Employee [] = [];
   displayedColumns: string[] = ['firstName', 'lastName', 'salary', 'edit', 'delete'];
-
-  constructor(private http: HttpClient) {}
+  
+  constructor(
+    private http: HttpClient,
+    private dialog: MatDialog
+    ) {}
 
   ngOnInit(){
-    this.http.get(environment.rootApi+"employees/")
-      .subscribe({
-        next: (res: any) => {
-          this.employeeList = res
-        error: () => {
-          // implement error toast?
-        }
-      }});
-  }
-  
-  editEmployee(any:any){
-    // placeholder
+    this.getEmployees();
   }
 
-  deleteEmployee(any:any){
+  private getEmployees() {
+    this.http.get(environment.rootApi+"employees/")
+    .subscribe({
+      next: (res: any) => {
+        this.employeeList = res
+      error: () => {
+        // implement error toast?
+      }
+    }});
+  }
+  
+  public addEmployee() {
+    const addDialogRef = this.dialog.open(EmployeeFormModalComponent, {
+      restoreFocus: false,
+      data: {
+        action: "Add"
+      }
+    })
+
+    addDialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let headers = new HttpHeaders();
+        headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+        this.http.post(environment.rootApi+"employees/create/", 
+          {
+            first_name: result.firstName,
+            last_name: result.lastName,
+            salary: result.salary
+          },
+          { 
+            headers: headers
+          }).subscribe({
+            next: (res: any) => {
+              this.getEmployees();
+              // implement success toast?
+            error: () => {
+              // implement error toast?
+            }
+          }});
+      } 
+    });
+  }
+
+  public editEmployee(employee: any){
+    const editDialogRef = this.dialog.open(EmployeeFormModalComponent, {
+      restoreFocus: false,
+      data: {
+        action: "Edit",
+        ...employee
+      }
+    })
+
+    editDialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let headers = new HttpHeaders();
+        headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+        this.http.put(environment.rootApi+`employees/${employee.id}/edit/`, 
+          {
+            first_name: result.firstName,
+            last_name: result.lastName,
+            salary: result.salary
+          },
+          { 
+            headers: headers
+          }).subscribe({
+            next: (res: any) => {
+              this.getEmployees();
+              // implement success toast?
+            error: () => {
+              // implement error toast?
+            }
+          }});
+      }
+    });
+  }
+
+  public deleteEmployee(any:any){
     // placeholder
   }
 
